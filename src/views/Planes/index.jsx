3f1620afb2e948c18363239bcbdb4f3fcb4941e3
antonioraper5612 <antonioraper5612@gmail.com>
2022-02-17 15:45:11 -0500

    update styles

--- src/views/Planes/index.jsx
+++ src/views/Planes/index.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import Header from '../../components/Header'
-import  {useNavigate}  from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 //Iconos
-// import velocidad from '../../Img/001-velocmetro.png'
-// import Destello from '../../Img/003-destello.png'
-// import musica from '../../Img/001-msica.png'
-// import videos from '../../Img/002-video.png'
+import velocidad from '../../Img/001-velocmetro.png'
+import Destello from '../../Img/003-destello.png'
+import musica from '../../Img/001-msica.png'
+import videos from '../../Img/002-video.png'
 import plan6Megas from '../../Img/6Megas.png'
 import plan8Megas from '../../Img/8megas.png'
 import plan10Megas from '../../Img/10megas.png'
@@ -15,16 +15,16 @@ import ImgPrincipal from '../../components/imgprincipal'
 
 const Planes = () => {
 
-const navigate=useNavigate()
+    const navigate = useNavigate()
 
-const handleaddplan =(plan)=>{
-    navigate(`/contact/${plan}`)
-}
+    const handleaddplan = (plan) => {
+        navigate(`/contact/${plan}`)
+    }
 
     return (
         <>
             <Header />
-            <ImgPrincipal/>
+            <ImgPrincipal />
             <div className='container container-title'>
                 <h2>Internet de última generación
                 </h2>
@@ -37,28 +37,36 @@ const handleaddplan =(plan)=>{
             </div>
             <div className='container container-content-rendimiento'>
                 <div className='content-redimiento'>
-                   <div className='spiner'></div>
+                    <div className='spiner'>
+                        <img src={velocidad} alt='velocidad'></img>
+                    </div>
                     <h2>Velocidad</h2>
                     <p>
                         Tienes la mayor velocidad de conexion a internet.</p>
                 </div>
                 <div className='content-redimiento'>
-                   
-                <div className='spiner'></div>
+
+                    <div className='spiner'>
+                        <img src={Destello} alt='velocidad'></img>
+                    </div>
                     <h2>Rendimiento</h2>
                     <p>
                         Contamos con la mejor tecnología para que nunca te detengas.
                     </p>
                 </div>
                 <div className='content-redimiento'>
-                <div className='spiner'></div>
+                    <div className='spiner'>
+                        <img src={videos} alt='velocidad'></img>
+                    </div>
                     <h2>Videos</h2>
                     <p>
                         Mira tus películas y videos las veces que desees y lo mejor sin cortes.
                     </p>
                 </div>
                 <div className='content-redimiento'>
-                <div className='spiner'></div>
+                    <div className='spiner'>
+                        <img src={musica} alt='velocidad'></img>
+                    </div>
                     <h2>Música</h2>
                     <p>
                         Escucha tu música sin parar y sin limites.
@@ -67,48 +75,48 @@ const handleaddplan =(plan)=>{
             </div>
             <div className='container container-content-plan'>
                 <div className='content-plan'>
-                    <img src={plan6Megas} alt='plan6Megas'/>
+                    <img src={plan6Megas} alt='plan6Megas' />
                     <h2>Plan Bronce</h2>
                     <p>6 Megas de bajada</p>
-                    <hr/>
+                    <hr />
                     <p>6 Megas de Subida</p>
-                    <hr/>
+                    <hr />
                     <p>Totalmente ilimitado</p>
                     <p>Hasta 5 dispositivos conectados
                         simultáneamente
                     </p>
                     <p><span>$55000</span></p>
-                     <button onClick={()=>handleaddplan('Plan Bronce')}>Lo Quiero Ya!</button>
+                    <button onClick={() => handleaddplan('Plan Bronce')}>Lo Quiero Ya!</button>
 
                 </div>
                 <div className='content-plan'>
-                    <img src={plan8Megas} alt='plan8Megas'/>
+                    <img src={plan8Megas} alt='plan8Megas' />
                     <h2>Plan Diamante</h2>
                     <p>8 Megas de bajada</p>
-                    <hr/>
+                    <hr />
                     <p>8 Megas de Subida</p>
-                    <hr/>
+                    <hr />
                     <p>Totalmente ilimitado</p>
                     <p>Hasta 10 dispositivos conectados
                         simultáneamente
                     </p>
-                     <p><span>$65000</span></p>
-                     <button onClick={()=>handleaddplan('Plan Diamante')}>Lo Quiero Ya!</button>
+                    <p><span>$65000</span></p>
+                    <button onClick={() => handleaddplan('Plan Diamante')}>Lo Quiero Ya!</button>
 
                 </div>
                 <div className='content-plan'>
-                    <img src={plan10Megas} alt='plan10Megas'/>
+                    <img src={plan10Megas} alt='plan10Megas' />
                     <h2>Plan Oro</h2>
                     <p>10 Megas de bajada</p>
-                    <hr/>
+                    <hr />
                     <p>10 Megas de Subida</p>
-                    <hr/>
+                    <hr />
                     <p>Totalmente ilimitado</p>
                     <p>Hasta 10 dispositivos conectados
                         simultáneamente
                     </p>
                     <p><span>$75000</span></p>
-                    <button onClick={()=>handleaddplan('Plan Oro')}>Lo Quiero Ya!</button>
+                    <button onClick={() => handleaddplan('Plan Oro')}>Lo Quiero Ya!</button>
                 </div>
             </div>
         </>
