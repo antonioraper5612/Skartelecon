2901ecc9c869541dada42b44f33fc89fa8609bfc
antonioraper5612 <antonioraper5612@gmail.com>
2022-02-17 19:13:23 -0500

    link new

--- src/views/ContentHome/index.jsx
+++ src/views/ContentHome/index.jsx
@@ -5,31 +5,40 @@ import axios from 'axios'
 import whatsapp from '../../Img/whatsapp.png'
 
 const ContentHome = () => {
-const navigate=useNavigate()
+    const navigate = useNavigate()
 
-    const handlenavigate=({target})=>{
-         const innerText =target.innerText
-         if(innerText ==='CAMBIATE YA!!'){
+    const handlenavigate = ({ target }) => {
+        const innerText = target.innerText
+        if (innerText === 'CAMBIATE YA!!') {
             navigate('/planes')
-         }else{
+        } else {
             navigate('/contact')
-         }
+        }
 
-    } 
+    }
 
-const hadlewhatsapp= async()=>{
-await axios.get('https://wa.me/3013454435')    
- }
+    const hadlewhatsapp = async () => {
+
+        const config =
+        {
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+            }
+        }
+
+        await axios.POST('https://wa.me/3013454435', config)
+    }
     return (
         <section className='img-Font'>
             <div className='section-title'>
                 <h2>Nunca un internet fue tan veloz</h2>
                 <p>Si pensaste que el internet de tu negocio o hogar nunca podría ser tan rápido es porque no has probado Skartelecon</p>
-                <button onClick={(e)=>handlenavigate(e)}>CAMBIATE YA!!</button>
-                <button onClick={(e)=>handlenavigate(e)}>CONTACTANOS</button>
+                <button onClick={(e) => handlenavigate(e)}>CAMBIATE YA!!</button>
+                <button onClick={(e) => handlenavigate(e)}>CONTACTANOS</button>
             </div>
             <div className='container-redes'>
-                <img src={whatsapp} alt='icon-whatsapp'  onClick={hadlewhatsapp} />
+                <img src={whatsapp} alt='icon-whatsapp' onClick={hadlewhatsapp} />
             </div>
         </section>
     )
